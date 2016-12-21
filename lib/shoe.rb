c17ec2f2ac5ee518ba3d073b35285625b15ad8df
davidamato8861 <davida8861@hstat.org>
2016-12-21 23:38:36 +0000

    finish Shoe hw

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,18 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   # create a class variable that will keep track of all of the brands
-  @@brands = []
+  
+  @@brands = [] #holds different instances of brands the user types
   # how will Shoe.brands be able to retrieve all of those brands?
   # REMEMBER: class methods start with self
   
-  def initialize(brand)
+  def initialize(brand) #uses global variables to hold all brands and all unique brands 
     @brand = brand
     @@brands << brand
     @@brands = @@brands.uniq
   end
   
-  def self.brands
-    @@brands
+  def self.brands #defines a method for each brand of shoe
+    @@brands #global variable for brands
   end
 
   # each new shoe that gets created MUST have a brand
