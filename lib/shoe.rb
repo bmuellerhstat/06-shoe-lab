af104788f00bd0581d0a08b46a1e303b068d6ca9
ryans4707 <ryans4707@hstat.org>
2016-12-23 03:38:34 +0000

    finish hw

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,10 +5,18 @@ class Shoe
   # create a class variable that will keep track of all of the brands
   @@brands = []
   
+  def initialize(brand)
+    @brand = brand
+    @@brands << brand
+    @@brands = @@brands.uniq
+  end
+  #.uniq 
   # each new shoe that gets created MUST have a brand
   # NOTE: unique brands should be added to the class variable of brands
   
-  
+  def self.brands
+    @@brands
+  end
   # how will Shoe.brands be able to retrieve all of those brands?
   # REMEMBER: class methods start with self
   
