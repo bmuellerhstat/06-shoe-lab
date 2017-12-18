359b4109a542733ea3c877dd0a2af73f6f7a6c2f
andrel6392 <andrel6392@hstat.org>
2017-12-18 14:21:44 +0000

    use conditional instead of .uniq

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -9,12 +9,15 @@ class Shoe
   # NOTE: unique brands should be added to the class variable of brands
   def initialize(brand)
     @brand = brand
-    @@brands << @brand
+    if @@brands.include?(brand) == false
+      @@brands << brand
+    end
   end
   
   # how will Shoe.brands be able to retrieve all of those brands?
   # REMEMBER: class methods start with self
   def self.brands
-    @@brands.uniq
+    # @@brands.uniq
+    @@brands
   end
 end
