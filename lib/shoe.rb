bf7dc2a92e4c246bca94e7bebc7b3c4fb241dc9e
davidk7680 <davidk7680@hstat.org>
2017-12-20 03:04:18 +0000

    finish rspec

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,27 @@ class Shoe
   attr_reader :brand
   
   # create a class variable that will keep track of all of the brands
+  @@brands = []
   
+  def initialize(brand)
+    @brands = brand
+    if @@brands.include?(brand) == false
+      @@brands << brand
+    end
+  end
+=begin  
+  def 
+    @@brands.map do |brands|
+      if @brand != brands
+        @@brands << @brand
+      end
+    end
+  end
+=end
+
+  def self.brands
+    @@brands
+  end
   
   # each new shoe that gets created MUST have a brand
   # NOTE: unique brands should be added to the class variable of brands
