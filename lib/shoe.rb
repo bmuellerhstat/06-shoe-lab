99f6186a6c4edaa54dbb6532072b06465524e787
Xavier Jiang <xavierj2216@hstat.org>
2018-12-07 22:54:41 +0000

    finish shoe lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,20 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  @@brands = []
   
   # create a class variable that will keep track of all of the brands
-  
+  def initialize(brand)
+    @brand = brand
+    @@brands << brand
+  end 
+   def brand=(brand)
+    @brand = brand
+    @@brands << brand
+  end 
+  def self.brands
+    @@brands.uniq
+  end
   
   # each new shoe that gets created MUST have a brand
   # NOTE: unique brands should be added to the class variable of brands
