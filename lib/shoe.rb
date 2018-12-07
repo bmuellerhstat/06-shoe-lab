fc74853d3b2a61a2fa2bf1175d7cf1f47248e472
Yu Hang Chen <yuhangc9321@hstat.org>
2018-12-07 22:38:32 +0000

    pass tests

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,6 +4,18 @@ class Shoe
   
   # create a class variable that will keep track of all of the brands
   
+  def initialize(brand)
+    @brand = brand
+    if @@brands.include?(brand) != true
+      @@brands << brand
+    end
+  end
+  
+  @@brands = []
+  
+  def self.brands
+    @@brands
+  end
   
   # each new shoe that gets created MUST have a brand
   # NOTE: unique brands should be added to the class variable of brands
