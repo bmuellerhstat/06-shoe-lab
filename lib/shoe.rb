0f07f0ef481d794b8287d0f283bf1b9bedba393d
Anabel Arbeeny <anabela0701@hstat.org>
2018-12-08 16:47:30 +0000

    rspec pass

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,30 @@
 class Shoe
+  
+  @@brands = []
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
   # create a class variable that will keep track of all of the brands
+  def initialize(brand)
+    @brand = brand
+    @@brands << brand
+    
+    if @@brands.include?(brand)
+      nil 
+    end
+    else
+      @@brands << brand 
+    end 
+  end 
   
+  def self.brands
+    @@brands 
+  end
+  
+  def brand=(brand)
+    @brand = brand 
+    
+  end  
   
   # each new shoe that gets created MUST have a brand
   # NOTE: unique brands should be added to the class variable of brands
