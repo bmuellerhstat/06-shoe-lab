dc4f24fb7f4f8c6613f17bda4cdb0a4cc2d11d42
Ahmad Rabah <ahmadr9279@hstat.org>
2018-12-10 07:26:00 +0000

    save work properly

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,15 +2,24 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
+  @@brands = []#array to keep all brands
   # create a class variable that will keep track of all of the brands
+  def initialize(brand)
+    @brand=brand
+    if @@brands.include?(brand)#if brand already exist delete the one already there, if not add it
+      @@brands.delete(brand)
+    else
+      @@brands << brand
+    end
   
+  def self.brands
+    @@brands
+  end
   
   # each new shoe that gets created MUST have a brand
   # NOTE: unique brands should be added to the class variable of brands
-  
-  
   # how will Shoe.brands be able to retrieve all of those brands?
   # REMEMBER: class methods start with self
   
-  
+end
 end
