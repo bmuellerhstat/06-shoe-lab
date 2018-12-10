085e32bcd60cc673493ed4c2a93c57a851df393e
Mohammed Amin <mohammeda6429@hstat.org>
2018-12-10 16:09:35 +0000

    done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,24 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+ attr_accessor :color, :size, :material, :condition
+attr_reader :brand
   
   # create a class variable that will keep track of all of the brands
+  
+  @@brands = []
+  
+  def initialize(brand)
+    @brand = brand
+    @@brands << @brand
+  end
   
   
   # each new shoe that gets created MUST have a brand
   # NOTE: unique brands should be added to the class variable of brands
+  def self.brands
+    @@brands.uniq
+  end
   
+    
   
   # how will Shoe.brands be able to retrieve all of those brands?
   # REMEMBER: class methods start with self
